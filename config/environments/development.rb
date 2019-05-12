e136d9188103f32ee623e8cfd9237e51808b8716
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-12 22:33:31 +0100

    minor fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,10 +5,10 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
+  config.action_mailer.perform_deliveries = true
   # Do not eager load code on boot.
   config.eager_load = false
-  config.action_mailer.delivery_method = :test 
+  config.action_mailer.delivery_method = :smtp 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Show full error reports.

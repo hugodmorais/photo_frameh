bb1cf31d7b351d8cec9e29885f2de2415e9c1116
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-13 22:21:57 +0100

    minor fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,15 @@
 # Load the Rails application.
-require File.expand_path('../application', __FILE__)
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net', 
-  :port => '587', 
-  :authentication => :plain, 
   :user_name => ENV['pusher_username'], 
   :password => ENV['pusher_password'], 
-  :domain => 'heroku.com', 
-  :enable_starttls_auto => true 
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
 }

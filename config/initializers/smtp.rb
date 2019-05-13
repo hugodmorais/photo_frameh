5cf853c77eaffe0aebba6f9c912039a2b3921d4f
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-13 22:01:02 +0100

    minor fix

--- config/initializers/smtp.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    domain: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    authentication: :login,
-    enable_starttls_auto: true
-  }
